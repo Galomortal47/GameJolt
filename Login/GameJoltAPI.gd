14b0376b45032b378995ca6661cf30179b656c15
Galomortal47 <44421928+Galomortal47@users.noreply.github.com>
2019-08-08 17:39:15 -0300

    update

--- Login/GameJoltAPI.gd
+++ Login/GameJoltAPI.gd
@@ -1,12 +1,15 @@
 extends HTTPRequest
-var myData = "key"
-var data = 150
+var myData = "key2"
+var data = 2500
 export var game_id = "377255"
 export var key_private = "791eef1ab8afcd2c70248a8a6e4cbe4a"
 var signature
 var url
 
 func _ready():
+	set_generate_signature()
+	set_data()
+	request(url)
 	get_generate_signature()
 	get_data()
 	request(url)
@@ -14,18 +17,18 @@ func _ready():
 
 func set_generate_signature():
 	signature = ("http://gamejolt.com/api/game/v1/data-store/set/?key=" + myData + "&&data=" + str(data) + "&format=json&game_id=" +  game_id + key_private).md5_text()
-	print(signature)
+#	print(signature)
 	
 func get_generate_signature():
-	signature = ("http://gamejolt.com/api/game/v1/data-store/get-keys/?format=json&game_id=" +  game_id + key_private).md5_text()
-	print(signature)
+	signature = ("http://gamejolt.com/api/game/v1/data-store/get-keys/?key="+ myData + "&format=json&game_id=" +  game_id + key_private).md5_text()
+#	print(signature)
 
 func set_data():
 	url = "http://gamejolt.com/api/game/v1/data-store/set/?key=" + myData + "&&data=" + str(data) + "&format=json&game_id=" +  game_id + "&signature=" + signature
 	print(url)
 
 func get_data():
-	url = "http://gamejolt.com/api/game/v1/data-store/get-keys/?format=json&game_id=" +  game_id + "&signature=" + signature
+	url = "http://gamejolt.com/api/game/v1/data-store/get-keys/?key="+ myData + "&format=json&game_id=" +  game_id + "&signature=" + signature
 	print(url)
 
 func _on_HTTPRequest_request_completed(result, response_code, headers, body):
