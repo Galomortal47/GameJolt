e388f2e8cc230bf39fec3c6c254c4a11610efd89
Galomortal47 <44421928+Galomortal47@users.noreply.github.com>
2019-08-08 18:02:06 -0300

    50 lines

--- Login/GameJoltAPI.gd
+++ Login/GameJoltAPI.gd
@@ -1,17 +1,19 @@
 extends HTTPRequest
-var myData = "key2"
-var data = 2500
+var myData = "savefile"
+var data = 3500
 export var game_id = "377255"
 export var key_private = "791eef1ab8afcd2c70248a8a6e4cbe4a"
 var signature
 var url
 
 func _ready():
-	set_generate_signature()
-	set_data()
-	request(url)
-	get_generate_signature()
-	get_data()
+#	set_generate_signature()
+#	set_data()
+#	request(url)
+#	get_generate_signature()
+#	get_data()
+	fetch_generate_signature()
+	fetch_data()
 	request(url)
 #	request("http://gamejolt.com/api/game/v1/data-store/set/?key=myData&data=100&format=json&game_id=377255&signature=6187cf6a77eaf8316643b3a720306658")
 
@@ -23,6 +25,10 @@ func get_generate_signature():
 	signature = ("http://gamejolt.com/api/game/v1/data-store/get-keys/?key="+ myData + "&format=json&game_id=" +  game_id + key_private).md5_text()
 #	print(signature)
 
+func fetch_generate_signature():
+	signature = ("http://gamejolt.com/api/game/v1/data-store/?format=json&game_id=" +  game_id + "&key=" + myData + key_private).md5_text()
+#	print(signature)
+
 func set_data():
 	url = "http://gamejolt.com/api/game/v1/data-store/set/?key=" + myData + "&&data=" + str(data) + "&format=json&game_id=" +  game_id + "&signature=" + signature
 	print(url)
@@ -30,9 +36,12 @@ func set_data():
 func get_data():
 	url = "http://gamejolt.com/api/game/v1/data-store/get-keys/?key="+ myData + "&format=json&game_id=" +  game_id + "&signature=" + signature
 	print(url)
+	
+func fetch_data():
+	url = "http://gamejolt.com/api/game/v1/data-store/?format=json&game_id=" +  game_id + "&key=" + myData + "&signature=" + signature
+	print(url)
 
 func _on_HTTPRequest_request_completed(result, response_code, headers, body):
 	var json = body.get_string_from_utf8()
-	print()
 	print(body.get_string_from_utf8())
 	pass # Replace with function body.
