db16b55514dd510b0b6e19326f059caf9992f76b
Galomortal47 <44421928+Galomortal47@users.noreply.github.com>
2019-08-09 18:13:12 -0300

    bug fixsw

--- Login/Login3t.gd
+++ Login/Login3t.gd
@@ -4,18 +4,23 @@ var data = {}
 var array = {}
 var i = 1
 var y = 1
+var account = false
 
 func _on_Button_button_down():
+	account = false
 	$HTTPRequest.fetched = false
-	$HTTPRequest.data = [$Username.get_text()]
+	$HTTPRequest.data = [$Password.get_text()]
+	$HTTPRequest.myData = str($Username.get_text())
 	$HTTPRequest.set_generate_signature()
 	$HTTPRequest.set_data()
 	var url = $HTTPRequest.url
 	$HTTPRequest.request(url)
+	$Label3.set_text("account created")
 	pass # Replace with function body.
 
 
 func _on_Button2_button_down():
+	account = true
 	$HTTPRequest.fetched = true
 	$HTTPRequest.fetch_generate_signature()
 	$HTTPRequest.fetch_data()
@@ -26,8 +31,10 @@ func _on_Button2_button_down():
 
 
 func _on_HTTPRequest_request_completed(result, response_code, headers, body):
-	if str($HTTPRequest.fetch) == str([$Username.get_text()]):
+	if str($HTTPRequest.fetch) == str([$Password.get_text()]):
+		$Label3.set_text("login succesfull")
 		print("succes")
-	else:
+	elif account:
+		$Label3.set_text("account doesen't exist")
 		print("failure")
 	pass # Replace with function body.
