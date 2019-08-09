b0aa6ecc0e88249fd2af093390414de454daa174
Galomortal47 <44421928+Galomortal47@users.noreply.github.com>
2019-08-09 16:57:05 -0300

    adding player movment

--- Login/Login3t.gd
+++ Login/Login3t.gd
@@ -20,6 +20,6 @@ func _on_Button2_button_down():
 	var url = $HTTPRequest.url
 	$HTTPRequest.request(url)
 #	print($HTTPRequest.json)
-	if str($HTTPRequest.json) == $Username.get_text():
+	if str($HTTPRequest.json.response.data) == str([$Username.get_text()]):
 		print("succes")
 	pass # Replace with function body.
--- multyplayer/Player.gd
+++ multyplayer/Player.gd
@@ -5,26 +5,41 @@ var speed = 50
 var max_speed = 500
 var drag = 0.85
 var jump = 75
+var air_drag = 0.95
 var air_time = 0.125
 var air_time_aux = air_time
+var speed_air = 20
 var gravity = 25
 
 func _process(delta):
 	if Input.is_action_pressed("ui_right"):
 		if motion.x < max_speed:
-			motion.x += speed
+			if floor_detection():
+				motion.x += speed_air
+			else:
+				motion.x += speed
 	elif Input.is_action_pressed("ui_left"):
 		if motion.x > -max_speed:
-			motion.x -= speed
+			if floor_detection():
+				motion.x -= speed
+			else:
+				motion.x -= speed_air
 	else:
-		motion.x *= drag
+		if floor_detection():
+			motion.x *= drag
+		else:
+			motion.x *= air_drag
 	if Input.is_action_pressed("ui_accept"):
 		if air_time > 0:
 			motion.y -= jump
 		air_time -= delta
-	if $floor_detect.is_colliding():
+	if floor_detection():
 		air_time = air_time_aux
 	else:
-		motion.y += gravity	
+		motion.y += gravity
 	motion = move_and_slide(motion)
 #	pass
+
+func floor_detection():
+	if $floor_detect.is_colliding() or $floor_detect2.is_colliding(): 
+		return true
